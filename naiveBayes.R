2a3a9a1378129b96807e60a0ba2168d2bbc32ece
Mikołaj Krzyszczak <miwo.krzyszczak@gmail.com>
2018-06-06 13:05:00 +0200

    Corrected NaiveBa

--- naiveBayes.R
+++ naiveBayes.R
@@ -26,20 +26,21 @@ validationSet<-testSet
 
 #Make factor if sober or abuse
 trainSet$ifAlco = "sober"
-trainSet[testSet$Dalc >= 3 & testSet$Walc >= 4,]$ifAlco = "abuse"
-trainSet[testSet$Dalc >= 4, ]$ifAlco = "abuse"
+trainSet[trainSet$Dalc >= 3 & trainSet$Walc>=4,]$ifAlco = "abuse"
+trainSet[trainSet$Dalc >= 4, ]$ifAlco = "abuse"
 #trainSet[trainSet$Walc < 4& trainSet$Dalc ]$ifAlco = "sober"
 trainSet$ifAlco = as.factor(trainSet$ifAlco)
-trainSet$Walc=NULL
 trainSet$Dalc=NULL
+trainSet$Walc=NULL
 
 validationSet$ifAlco = "sober"
 validationSet[validationSet$Dalc >= 3 & validationSet$Walc >=4, ]$ifAlco = "abuse"
 validationSet[validationSet$Dalc >= 4,]$ifAlco = "abuse"
 validationSet$ifAlco = as.factor(validationSet$ifAlco)
 
-testSet$Walc=NULL
 testSet$Dalc=NULL
+testSet$Walc=NULL
+
 
 # Do Naive Bayes classification.
 nb = NaiveBayes(ifAlco ~ .,data=trainSet)
