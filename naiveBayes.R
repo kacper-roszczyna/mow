8a35d5591e7e5f9eb6c00c0a434355a2740d5b44
Mikołaj Krzyszczak <miwo.krzyszczak@gmail.com>
2018-06-06 12:50:22 +0200

    Changed NaiveBayes

--- naiveBayes.R
+++ naiveBayes.R
@@ -2,27 +2,49 @@
 #install.packages("klaR")
 #Loading the library
 library(klaR)
+library(magrittr)
+library(dplyr)
 
-trainSet<-d1
-testSet<-d2
-validationSet<-d2
 
-#Make factor if sober or drunk
+d3=rbind(d1,d2)
+d3<-d3 %>% distinct(school,sex,age,address,famsize,Pstatus,
+                             Medu,Fedu,Mjob,Fjob,reason,
+                             guardian,traveltime,studytime,failures,
+                             schoolsup, famsup,activities,nursery,higher,internet,
+                             romantic,famrel,freetime,goout,Dalc,Walc,health,absences, .keep_all = TRUE)
+
+
+
+##Dividing set into train and test data
+set.seed(123)
+smp_size <- floor(0.8 * nrow(d3))
+train_ind <- sample(seq_len(nrow(d3)), size = smp_size)
+
+trainSet<-d3[train_ind, ]
+testSet<-d3[-train_ind, ]
+validationSet<-testSet
+
+#Make factor if sober or abuse
 trainSet$ifAlco = "sober"
-trainSet[trainSet$Dalc > 1,]$ifAlco = "drunk"
-trainSet[trainSet$Walc > 2,]$ifAlco = "drunk"
+trainSet[testSet$Dalc >= 3 & testSet$Walc >= 4,]$ifAlco = "abuse"
+trainSet[testSet$Dalc >= 4, ]$ifAlco = "abuse"
+#trainSet[trainSet$Walc < 4& trainSet$Dalc ]$ifAlco = "sober"
 trainSet$ifAlco = as.factor(trainSet$ifAlco)
 trainSet$Walc=NULL
 trainSet$Dalc=NULL
 
 validationSet$ifAlco = "sober"
-validationSet[validationSet$Dalc > 1,]$ifAlco = "drunk"
-validationSet[validationSet$Walc > 2,]$ifAlco = "drunk"
+validationSet[validationSet$Dalc >= 3 & validationSet$Walc >=4, ]$ifAlco = "abuse"
+validationSet[validationSet$Dalc >= 4,]$ifAlco = "abuse"
 validationSet$ifAlco = as.factor(validationSet$ifAlco)
 
+testSet$Walc=NULL
+testSet$Dalc=NULL
 
 # Do Naive Bayes classification.
 nb = NaiveBayes(ifAlco ~ .,data=trainSet)
+
+# Do prediction on testset
 nb_predict=predict(nb, testSet)
 
 #Perform TP TN FP FN table
